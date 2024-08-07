169b6f4ebc219a0c9421c87754f64d9f3f7e6ccf
Fer <lfernando2091@gmail.com>
2024-08-06 20:57:05 -0600

    Updated JSON Schema gpt-3.5-4o.schema.json (#4)
    * Updated schema gpt-3.5-4o.schema.json
    Added new schema gpt-babbage-davinci-002.schema.json
    
    * Updated conditional validation for gpts 3.5 and 4o schemas
    
    * Added schema test validators for gpts 3.5 and 4o schemas
    
    * Added more test scenarios for gpt-3.5-4o.schema.json schema file
    
    * added pr checks
    
    * updated CI yml
    
    * updated CI yml
    
    * updated CI yml
    
    * updated CI yml
    
    * updated ci test location folder

--- project/src/__tests__/gpt-3.5-4o-schema.spec.ts
+++ project/src/__tests__/gpt-3.5-4o-schema.spec.ts
@@ -2,9 +2,19 @@ import Ajv from 'ajv/dist/2020'
 import { GPT_305_AND_4O_SCHEMA } from "../json-schema/schema";
 import { EMPTY_OBJECT } from "../stubs";
 import {
+    ASSISTANT_MESSAGES_WITH_WEIGHTS,
     ASSISTANT_MISSING_CONTENT_MSG,
-    ASSISTANT_WITH_FUNC_CALL, ASSISTANT_WITH_FUNC_CALL_AND_CONTENT, MESSAGE_PROPERTY_EMPTY,
+    ASSISTANT_WITH_FUNC_CALL,
+    ASSISTANT_WITH_FUNC_CALL_AND_CONTENT, EXTRA_PROPERTY,
+    FUNCTION_MISSING_CONTENT,
+    FUNCTION_MISSING_NAME,
+    FUNCTION_MISSING_NAME_AND_CONTENT,
+    MESSAGE_PROPERTY_EMPTY,
+    MESSAGES_WITH_EMPTY_FUNC_LIST, MESSAGES_WITH_FUNC_ITEM_WITH_NAME_DESCRIPTION_ONLY,
+    MESSAGES_WITH_FUNC_ITEM_WITH_NAME_ONLY,
+    MESSAGES_WITH_FUNC_LIST,
     ONLY_SYSTEM_MESSAGE,
+    REPEATED_MESSAGES,
     SIMPLE_VALID_MESSAGE
 } from '../stubs/gpt-3.5-4o-schema.stubs';
 
@@ -18,7 +28,7 @@ describe('Gpt 3.5 and 4o schema tests', () => {
         expect(valid).toBe(true);
     });
 
-    it('Valid assistant with function call and `content` property removed', () => {
+    it('Valid assistant with function call and missing `content` property', () => {
         const valid = validate(ASSISTANT_WITH_FUNC_CALL);
         expect(validate.errors).toBe(null);
         expect(valid).toBe(true);
@@ -28,6 +38,54 @@ describe('Gpt 3.5 and 4o schema tests', () => {
         const valid = validate(ASSISTANT_WITH_FUNC_CALL_AND_CONTENT);
         expect(validate.errors).toBe(null);
         expect(valid).toBe(true);
+    });
+
+    it('Valid assistant with function call and `functions` list', () => {
+        const valid = validate(MESSAGES_WITH_FUNC_LIST);
+        expect(validate.errors).toBe(null);
+        expect(valid).toBe(true);
+    });
+
+    it('Valid assistant messages with weights definitions', () => {
+        const valid = validate(ASSISTANT_MESSAGES_WITH_WEIGHTS);
+        expect(validate.errors).toBe(null);
+        expect(valid).toBe(true);
+    });
+
+    it('Missing `function` -> `content` property', () => {
+        const valid = validate(FUNCTION_MISSING_CONTENT)
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/messages/2");
+        expect(errors[0].schemaPath).toBe('#/properties/messages/items/allOf/0/then/required');
+        expect(errors[0].message).toBe("must have required property 'content'");
+        expect(errors[0].params["missingProperty"]).toBe("content");
+        expect(errors[0].keyword).toBe("required");
+        expect(valid).toBe(false);
+    });
+
+    it('Missing `function` -> `name` property', () => {
+        const valid = validate(FUNCTION_MISSING_NAME)
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/messages/2");
+        expect(errors[0].schemaPath).toBe('#/properties/messages/items/allOf/0/then/required');
+        expect(errors[0].message).toBe("must have required property 'name'");
+        expect(errors[0].params["missingProperty"]).toBe("name");
+        expect(errors[0].keyword).toBe("required");
+        expect(valid).toBe(false);
+    });
+
+    it('Missing `function` -> `name` and `content` properties', () => {
+        const valid = validate(FUNCTION_MISSING_NAME_AND_CONTENT)
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/messages/2");
+        expect(errors[0].schemaPath).toBe('#/properties/messages/items/allOf/0/then/required');
+        expect(errors[0].message).toBe("must have required property 'content'");
+        expect(errors[0].params["missingProperty"]).toBe("content");
+        expect(errors[0].keyword).toBe("required");
+        expect(valid).toBe(false);
     });
 
     it('Missing `assistant` -> `content` property', () => {
@@ -54,7 +112,7 @@ describe('Gpt 3.5 and 4o schema tests', () => {
         expect(valid).toBe(false);
     });
 
-    it('Property `messages` empty elements', () => {
+    it('Invalid property `messages` empty elements', () => {
         const valid = validate(MESSAGE_PROPERTY_EMPTY)
         const errors = validate.errors;
         expect(errors.length).toBe(1);
@@ -66,7 +124,7 @@ describe('Gpt 3.5 and 4o schema tests', () => {
         expect(valid).toBe(false);
     });
 
-    it('Only one `messages` element', () => {
+    it('Invalid only one `messages` element', () => {
         const valid = validate(ONLY_SYSTEM_MESSAGE)
         const errors = validate.errors;
         expect(errors.length).toBe(1);
@@ -78,5 +136,64 @@ describe('Gpt 3.5 and 4o schema tests', () => {
         expect(valid).toBe(false);
     });
 
+    it('Invalid repeated messages', () => {
+        const valid = validate(REPEATED_MESSAGES);
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/messages");
+        expect(errors[0].schemaPath).toBe("#/properties/messages/uniqueItems");
+        expect(errors[0].message).toBe( "must NOT have duplicate items (items ## 2 and 5 are identical)");
+        expect(errors[0].params["i"]).toBe(5);
+        expect(errors[0].params["j"]).toBe(2);
+        expect(errors[0].keyword).toBe("uniqueItems");
+        expect(valid).toBe(false);
+    });
 
+    it('Invalid empty `functions` list', () => {
+        const valid = validate(MESSAGES_WITH_EMPTY_FUNC_LIST);
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/functions");
+        expect(errors[0].schemaPath).toBe('#/properties/functions/minItems');
+        expect(errors[0].message).toBe( 'must NOT have fewer than 1 items');
+        expect(errors[0].params["limit"]).toBe(1);
+        expect(errors[0].keyword).toBe("minItems");
+        expect(valid).toBe(false);
+    });
+
+    it('Invalid `functions` item with only `name` property', () => {
+        const valid = validate(MESSAGES_WITH_FUNC_ITEM_WITH_NAME_ONLY);
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/functions/0");
+        expect(errors[0].schemaPath).toBe('#/properties/functions/items/required');
+        expect(errors[0].message).toBe( "must have required property 'description'");
+        expect(errors[0].params["missingProperty"]).toBe('description');
+        expect(errors[0].keyword).toBe("required");
+        expect(valid).toBe(false);
+    });
+
+    it('Invalid `functions` item with only `name` and `description` property', () => {
+        const valid = validate(MESSAGES_WITH_FUNC_ITEM_WITH_NAME_DESCRIPTION_ONLY);
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("/functions/0");
+        expect(errors[0].schemaPath).toBe('#/properties/functions/items/required');
+        expect(errors[0].message).toBe( "must have required property 'parameters'");
+        expect(errors[0].params["missingProperty"]).toBe('parameters');
+        expect(errors[0].keyword).toBe("required");
+        expect(valid).toBe(false);
+    });
+
+    it('Only allow `messages` and `functions` properties', () => {
+        const valid = validate(EXTRA_PROPERTY);
+        const errors = validate.errors;
+        expect(errors.length).toBe(1);
+        expect(errors[0].instancePath).toBe("");
+        expect(errors[0].schemaPath).toBe('#/additionalProperties');
+        expect(errors[0].message).toBe( 'must NOT have additional properties');
+        expect(errors[0].params["additionalProperty"]).toBe('newProperty');
+        expect(errors[0].keyword).toBe("additionalProperties");
+        expect(valid).toBe(false);
+    });
 });
